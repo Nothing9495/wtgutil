8ad7ad6f84ff8bb46cf3e522489d147eced276ce
Charles Yan <nothing-6y@outlook.com>
2022-10-16 13:52:58 +0800

    New - Check partmgr's "SanPolicy" configuration New - Hide or show local disks etc.

--- WTG Switcher/Program.cs
+++ WTG Switcher/Program.cs
@@ -12,24 +12,28 @@ namespace WTG_Switcher
         {
             //Apperance
             string Time = DateTime.Now.ToString("HH:mm:ss");
-            Console.Title = ("WTG Switcher v2 " + "(Start Time: " + Time + ")");
+            Console.Title = ("WTG Switcher v3 " + "(Start Time: " + Time + ")");
             Console.WriteLine();
-            Console.WriteLine("WTG Switcher Release v2");
+            Console.WriteLine("WTG Switcher v3");
             Console.WriteLine("Copyright (C) Charles.");
             //Check Status
             ////Get GUID
-            RegistryKey BDF = Registry.LocalMachine.OpenSubKey("SYSTEM\\HardwareConfig");
+            RegistryKey BDF = Registry.LocalMachine.OpenSubKey("SYSTEM\\HardwareConfig\\Current");
             object BR = BDF.GetValue("BootDriverFlags");
             RegistryKey POS = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control");
             object PR = POS.GetValue("PortableOperatingSystem");
+            RegistryKey PGR = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\partmgr\\Parameters");
+            object PS = PGR.GetValue("SanPolicy");
             ////Output
             Console.WriteLine();
             Console.WriteLine("Currnet Status:" +
-                Environment.NewLine + " Boot Driver Settings: " + BR + 
-                Environment.NewLine + " PortableOS Feature: " + PR);
+                Environment.NewLine + " Boot Driver Settings: " + BR +
+                Environment.NewLine + " PortableOS Feature: " + PR +
+                Environment.NewLine + " Partmgr Settings: " + PS );
             Console.WriteLine();
-            Console.WriteLine("     For Boot Driver Settings, 20 means USB mode, 0 means Local Disk mode" + 
-                Environment.NewLine + "     For PortableOS Feature, 1 means enbale, 0 means disbaled.");
+            Console.WriteLine("     Boot Driver Settings: 20 - USB mode, 0 - Non-USB mode" + 
+                Environment.NewLine + "     PortableOS Feature: 1 - enbaled, 0 - disbaled." + 
+                Environment.NewLine + "     Partmgr Settings: 1 - Default settings; 4 - Hide local disks");
 
 
             //Menu
@@ -38,8 +42,9 @@ namespace WTG_Switcher
             Console.WriteLine();
             Console.WriteLine("Which operation do you want to perform?");
             Console.WriteLine("1. Swtich to WTG Mode" +
-               Environment.NewLine + "2. Switch to Normal Mode" +
-               Environment.NewLine + "3. Exit");
+               Environment.NewLine + "2. Switch to Normal Mode" + 
+               Environment.NewLine + "3. Show local disks" + 
+               Environment.NewLine + "4. Exit");
 
             Console.WriteLine();
             var input = Console.ReadKey();
@@ -64,10 +69,12 @@ namespace WTG_Switcher
         private static void RouteChoice(int Choice)
         {
         //Switch
-        string Key1 = "SYSTEM\\HardwareConfig";
+        string Key1 = "SYSTEM\\HardwareConfig\\Current";
         string BDF = "BootDriverFlags";
         string Key2 = "SYSTEM\\CurrentControlSet\\Control";
         string POS = "PortableOperatingSystem";
+        string Key3 = "SYSTEM\\CurrentControlSet\\Services\\partmgr\\Parameters";
+        string SAN = "SanPolicy";
 
             switch (Choice)
             {
@@ -79,9 +86,13 @@ namespace WTG_Switcher
                     RegistryKey Portable = Registry.LocalMachine.CreateSubKey(Key2);
                     Portable.SetValue(POS, 1, RegistryValueKind.DWord);
                     Portable.Close();
+                    RegistryKey PartmgrHide = Registry.LocalMachine.CreateSubKey(Key3);
+                    PartmgrHide.SetValue(SAN, 4, RegistryValueKind.DWord);
+                    PartmgrHide.Close();
 
                     Console.WriteLine();
                     Console.WriteLine("Done.");
+                    Console.WriteLine("Restart to make some of the changes take effect" + Environment.NewLine);
                     Console.Write("Press any key to exit...");
                     Console.ReadKey(true);
                     Console.WriteLine();
@@ -96,9 +107,28 @@ namespace WTG_Switcher
                     RegistryKey Normal = Registry.LocalMachine.CreateSubKey(Key2);
                     Normal.SetValue(POS, 0, RegistryValueKind.DWord);
                     Normal.Close();
+                    RegistryKey PartmgrShow = Registry.LocalMachine.CreateSubKey(Key3);
+                    PartmgrShow.SetValue(SAN, 1, RegistryValueKind.DWord);
+                    PartmgrShow.Close();
 
                     Console.WriteLine();
                     Console.WriteLine("Done.");
+                    Console.WriteLine("Restart to make some of the changes take effect" + Environment.NewLine);
+                    Console.Write("Press any key to exit...");
+                    Console.ReadKey(true);
+                    Console.WriteLine();
+                    Process.GetCurrentProcess().Kill();
+                    break;
+
+                //Show local disks
+                case 3:
+                    RegistryKey Partmgr = Registry.LocalMachine.CreateSubKey(Key3);
+                    Partmgr.SetValue(SAN, 1, RegistryValueKind.DWord);
+                    Partmgr.Close();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Done.");
+                    Console.WriteLine("Restart to apply the change." + Environment.NewLine);
                     Console.Write("Press any key to exit...");
                     Console.ReadKey(true);
                     Console.WriteLine();
